ba427fcee9dde6133a93ad36c7f43485b452619c
SewerynKaminski <Seweryn.kaminski@gmail.com>
2021-12-16 20:33:38 +0100

    2021 day 13

---
+++ year2021/day13/day_13.cpp
@@ -0,0 +1,131 @@
+//---------------------------------------------------------------------------//
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <ranges>
+#include <fstream>
+#include <limits>
+#include <set>
+
+//---------------------------------------------------------------------------//
+#include "day_13.h"
+
+//---------------------------------------------------------------------------//
+namespace aoc::year_2021::day_13 {
+
+//---------------------------------------------------------------------------//
+template<class T>
+constexpr auto Range ( T a, T b ) {
+    return std::views::iota ( a, b );
+}
+//---------------------------------------------------------------------------//
+struct Point {
+    int x, y;
+};
+
+//---------------------------------------------------------------------------//
+auto load ( std::string file ) {
+    std::fstream fs ( file );
+    std::vector<Point> data;
+    std::vector<std::tuple<char, int>> fold;
+
+    std::string line;
+    // punkty
+    while ( fs.good() ) {
+        std::getline ( fs, line );
+        if ( !line.empty() ) {
+            std::stringstream ss ( line );
+            int x, y;
+            char c;
+            ss >> x >> c >> y;
+            data.push_back ( {x, y} );
+        } else {
+            break;
+        }
+    }
+    // zlozenia
+    while ( fs.good() ) {
+        std::getline ( fs, line );
+        if ( !line.empty() ) {
+            char c;
+            int v;
+            std::stringstream ss ( line );
+            ss.seekg ( 11 ) >> c;
+            ss.seekg ( 13 ) >> v;
+            fold.push_back ( {c, v} );
+        }
+    }
+
+    return std::tuple{data, fold};
+}
+
+//---------------------------------------------------------------------------//
+void Task_1 ( ) {
+    auto ans = 0;
+    auto [data, fold] = load ( "../input" );
+
+    auto [a, v] = fold[0];
+    for ( auto& p : data ) {
+        if ( a == 'x' ) {
+            if ( p.x > v ) {
+                p.x = 2 * v - p.x;
+            }
+        } else {
+            if ( p.y > v ) {
+                p.y = 2 * v - p.y;
+            }
+        }
+    }
+
+    std::set<int> paper;
+    for ( auto& [x, y] : data ) {
+        paper.insert ( ( x << 16 ) | y );
+    }
+    ans = paper.size();
+
+    std::cout << __FUNCTION__ << ": " << ans << '\n';
+}
+
+//---------------------------------------------------------------------------//
+void Task_2 ( ) {
+    auto [data, fold] = load ( "../input" );
+
+    for ( auto [a, v] : fold )
+        for ( auto& p : data ) {
+            if ( a == 'x' ) {
+                if ( p.x > v ) {
+                    p.x = 2 * v - p.x;
+                }
+            } else {
+                if ( p.y > v ) {
+                    p.y = 2 * v - p.y;
+                }
+            }
+        }
+
+    auto minx = 0;
+    auto maxx = 0;
+    auto miny = 0;
+    auto maxy = 0;
+    for ( auto[x, y] : data ) {
+        minx = std::min ( minx, x );
+        maxx = std::max ( maxx, x );
+        miny = std::min ( miny, y );
+        maxy = std::max ( maxy, y );
+    }
+
+    std::vector<std::string> img ( maxy + 1 );
+    for ( auto& s : img ) s.resize ( maxx + 1, ' ' );
+    for ( auto& [x, y] : data ) {
+        img[y][x] = '#';
+    }
+    for ( auto& s : img )
+        std::cout << s << '\n';
+
+    std::cout << __FUNCTION__ << ":" << '\n';
+}
+
+}
---
+++ year2021/day13/day_13.h
@@ -0,0 +1,7 @@
+#ifndef DAY_13_H
+#define DAY_13_H
+namespace aoc::year_2021::day_13 {
+void Task_1();
+void Task_2();
+}
+#endif
