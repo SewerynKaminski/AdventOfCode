f305864de6400cfb134b292de8a76f034f0fd63b
Seweryn <seweryn.kaminski@gmail.com>
2024-12-07 21:42:45 +0100

    Delete year2024/day07/Release directory

--- year2024/day07/Release/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_EXTRA_GENERATOR" "CodeBlocks" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/lib/qt5/bin/qmake" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc-10" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++-10" CACHE "STRING" "" FORCE)
-set("CMAKE_GENERATOR" "Unix Makefiles" CACHE "STRING" "" FORCE)
