65ee6795d526e930ce07e57b1efccf17b1ed2c22
SewerynKaminski <Seweryn.kaminski@gmail.com>
2024-12-14 18:23:02 +0100

    Remove ndo not used includes

--- year2024/day14/day_14.cpp
+++ year2024/day14/day_14.cpp
@@ -1,9 +1,5 @@
 #include "day"
-#include <cmath>
-#include <climits>
-#include <format>
-#include <unistd.h>
-#include <termios.h>
+
 //---------------------------------------------------------------------------//
 namespace aoc::YEAR::DAY {
 
@@ -108,12 +104,12 @@ void Task_1 ( ::std::istream& puzzle_input ) {
 }
 
 //---------------------------------------------------------------------------//
-void enableRawMode() {
+/*void enableRawMode() {
     struct termios raw;
     tcgetattr(STDIN_FILENO, &raw);
     raw.c_lflag &= ~(ECHO| ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
+}*/
 
 //---------------------------------------------------------------------------//
 void Task_2 ( ::std::istream& puzzle_input ) {
